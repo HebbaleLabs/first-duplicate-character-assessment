2220d4c06fe9af4d68e55ff089fa636bc70b6a79
haripriyasree <haripriya.s@hebbalelabs.com>
2019-08-16 19:41:59 +0530

    quick changes

---
+++ src/main/java/com/talfinder/assessment/FirstNonDuplicateCharacterAssessment.java
@@ -0,0 +1,25 @@
+package com.talfinder.assessment;
+
+
+public class FirstNonDuplicateCharacterAssessment {
+
+
+    static Character firstNonRepeatingCharacter(String word) {
+
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+
+        return Character.MIN_VALUE;
+    }
+
+
+    public static void main(String[] args) {
+        String word = "hello";
+        System.out.println((firstNonRepeatingCharacter(word)));
+    }
+}
---
+++ src/test/java/com/talfinder/assessment/FirstNonDuplicateCharacterAssessmentTest.java
@@ -0,0 +1,42 @@
+package com.talfinder.assessment;
+
+import com.talfinder.assessment.FirstNonDuplicateCharacterAssessment;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@RunWith(Parameterized.class)
+public class FirstNonDuplicateCharacterAssessmentTest {
+    String text;
+    Character expected;
+
+    public FirstNonDuplicateCharacterAssessmentTest(String text, Character expected) {
+        this.text = text;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(new Object[][]{
+                {"apple", 'a'},
+                {"reed", 'r'},
+                {"you are my fault", 'o'},
+                {"india", 'n'},
+                {"elle", null},
+                {"Abba", 'A'},
+                {"e$&yh*eh", 'y'},
+                {"e45th4e", '5'},
+                {"if the input is apple, then the output would be a If the input is racecars, then the output would be ‘e’", 'I'},
+                {"aromatic", 'r'}});
+    }
+
+    @Test
+    public void checkForNonDuplicateCharacter() {
+        Assert.assertEquals("input:String as " + text + "", expected, FirstNonDuplicateCharacterAssessment.firstNonRepeatingCharacter(text));
+    }
+}
